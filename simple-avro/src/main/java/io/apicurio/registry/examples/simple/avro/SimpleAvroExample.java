12418a8669be29a4232e3e91645682dc003a56d3
Eric Wittmann <eric.wittmann@gmail.com>
2020-09-25 12:36:27 -0400

    created the first (simple) avro example

---
+++ simple-avro/src/main/java/io/apicurio/registry/examples/simple/avro/SimpleAvroExample.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2020 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.registry.examples.simple.avro;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import io.apicurio.registry.utils.serde.AbstractKafkaSerDe;
+import io.apicurio.registry.utils.serde.AbstractKafkaSerializer;
+import io.apicurio.registry.utils.serde.AvroKafkaDeserializer;
+import io.apicurio.registry.utils.serde.AvroKafkaSerializer;
+import io.apicurio.registry.utils.serde.strategy.GetOrCreateIdStrategy;
+import io.apicurio.registry.utils.serde.strategy.SimpleTopicIdStrategy;
+
+/**
+ * This example demonstrates how to use the Apicurio Registry in a very simple publish/subscribe
+ * scenario with Avro as the serialization type.  The following aspects are demonstrated:
+ * 
+ * <ol>
+ *   <li>Configuring a Kafka Serializer for use with Apicurio Registry</li>
+ *   <li>Configuring a Kafka Deserializer for use with Apicurio Registry</li>
+ *   <li>Auto-register the Avro schema in the registry (registered by the producer)</li>
+ *   <li>Data sent as a simple GenericRecord, no java beans needed</li>
+ * </ol>
+ * 
+ * Pre-requisites:
+ * 
+ * <ul>
+ *   <li>Kafka must be running on localhost:9092</li>
+ *   <li>Apicurio Registry must be running on localhost:8080</li>
+ * </ul>
+ * 
+ * @author eric.wittmann@gmail.com
+ */
+public class SimpleAvroExample {
+    
+    private static final String REGISTRY_URL = "http://localhost:8080/api";
+    private static final String SERVERS = "localhost:9092";
+    private static final String TOPIC_NAME = SimpleAvroExample.class.getSimpleName();
+    private static final String SUBJECT_NAME = "Greeting";
+    private static final String AVRO_SCHEMA = "{\"type\":\"record\",\"name\":\"Greeting\",\"fields\":[{\"name\":\"Message\",\"type\":\"string\"},{\"name\":\"Time\",\"type\":\"long\"}]}";
+
+    
+    public static final void main(String [] args) throws Exception {
+        System.out.println("Starting example " + SimpleAvroExample.class.getSimpleName());
+        String topicName = TOPIC_NAME;
+        String subjectName = SUBJECT_NAME;
+
+        // Create the producer.
+        Producer<Object, Object> producer = createKafkaProducer();
+        // Produce 5 messages.
+        int producedMessages = 0;
+        try {
+            Schema schema = new Schema.Parser().parse(AVRO_SCHEMA);
+            System.out.println("Producing (5) messages.");
+            for (int idx = 0; idx < 5; idx++) {
+                // Use the schema to create a record
+                GenericRecord record = new GenericData.Record(schema);
+                Date now = new Date();
+                String message = "Hello (" + producedMessages++ + ")!";
+                record.put("Message", message);
+                record.put("Time", now.getTime());
+                
+                // Send/produce the message on the Kafka Producer
+                ProducerRecord<Object, Object> producedRecord = new ProducerRecord<>(topicName, subjectName, record);
+                producer.send(producedRecord);
+                
+                Thread.sleep(100);
+            }
+            System.out.println("Messages successfully produced.");
+        } finally {
+            System.out.println("Closing the producer.");
+            producer.flush();
+            producer.close();
+        }
+        
+        // Create the consumer
+        System.out.println("Creating the consumer.");
+        KafkaConsumer<Long, GenericRecord> consumer = createKafkaConsumer();
+
+        // Subscribe to the topic
+        System.out.println("Subscribing to topic " + topicName);
+        consumer.subscribe(Collections.singletonList(topicName));
+
+        // Consume the 5 messages.
+        try {
+            int messageCount = 0;
+            System.out.println("Consuming (5) messages.");
+            while (messageCount < 5) {
+                final ConsumerRecords<Long, GenericRecord> records = consumer.poll(Duration.ofSeconds(1));
+                messageCount += records.count();
+                if (records.count() == 0) {
+                    // Do nothing - no messages waiting.
+                    System.out.println("No messages waiting...");
+                } else records.forEach(record -> {
+                    GenericRecord value = record.value();
+                    System.out.println("Consumed a message: " + value.get("Message") + " @ " + new Date((long) value.get("Time")));
+                });
+            }
+        } finally {
+            consumer.close();
+        }
+        
+        System.out.println("Done (success).");
+    }
+
+    /**
+     * Creates the Kafka producer.
+     */
+    private static Producer<Object, Object> createKafkaProducer() {
+        Properties props = new Properties();
+
+        // Configure kafka settings
+        props.putIfAbsent(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
+        props.putIfAbsent(ProducerConfig.CLIENT_ID_CONFIG, "Producer-" + TOPIC_NAME);
+        props.putIfAbsent(ProducerConfig.ACKS_CONFIG, "all");
+        props.putIfAbsent(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // Use the Apicurio Registry provided Kafka Serializer for Avro
+        props.putIfAbsent(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroKafkaSerializer.class.getName());
+
+        // Configure Service Registry location
+        props.putIfAbsent(AbstractKafkaSerDe.REGISTRY_URL_CONFIG_PARAM, REGISTRY_URL);
+        // Map the topic name to the artifactId in the registry
+        props.putIfAbsent(AbstractKafkaSerializer.REGISTRY_ARTIFACT_ID_STRATEGY_CONFIG_PARAM, SimpleTopicIdStrategy.class.getName());
+        // Get an existing schema or auto-register if not found
+        props.putIfAbsent(AbstractKafkaSerializer.REGISTRY_GLOBAL_ID_STRATEGY_CONFIG_PARAM, GetOrCreateIdStrategy.class.getName());
+
+        // Create the Kafka producer
+        Producer<Object, Object> producer = new KafkaProducer<>(props);
+        return producer;
+    }
+
+    /**
+     * Creates the Kafka consumer.
+     */
+    private static KafkaConsumer<Long, GenericRecord> createKafkaConsumer() {
+        Properties props = new Properties();
+
+        // Configure Kafka
+        props.putIfAbsent(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
+        props.putIfAbsent(ConsumerConfig.GROUP_ID_CONFIG, "Consumer-" + TOPIC_NAME);
+        props.putIfAbsent(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.putIfAbsent(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.putIfAbsent(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // Use the Apicurio Registry provided Kafka Deserializer for Avro
+        props.putIfAbsent(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, AvroKafkaDeserializer.class.getName());
+
+        // Configure Service Registry location
+        props.putIfAbsent(AbstractKafkaSerDe.REGISTRY_URL_CONFIG_PARAM, REGISTRY_URL);
+        // No other configuration needed for the deserializer, because the globalId of the schema
+        // the deserializer should use is sent as part of the payload.  So the deserializer simply
+        // extracts that globalId and uses it to look up the Schema from the registry.
+
+        // Create the Kafka Consumer
+        KafkaConsumer<Long, GenericRecord> consumer = new KafkaConsumer<>(props);
+        return consumer;
+    }
+
+}
