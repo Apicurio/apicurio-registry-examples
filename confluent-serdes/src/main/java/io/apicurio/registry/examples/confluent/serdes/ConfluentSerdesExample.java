02396103f93cc60641e592b43432ce83150098d0
Eric Wittmann <eric.wittmann@gmail.com>
2021-02-24 10:45:08 -0500

    fixed a URL

--- confluent-serdes/src/main/java/io/apicurio/registry/examples/confluent/serdes/ConfluentSerdesExample.java
+++ confluent-serdes/src/main/java/io/apicurio/registry/examples/confluent/serdes/ConfluentSerdesExample.java
@@ -63,7 +63,7 @@ import io.confluent.kafka.serializers.KafkaAvroSerializer;
 public class ConfluentSerdesExample {
 
     private static final String REGISTRY_URL = "http://localhost:8080/apis/registry/v2";
-    private static final String CCOMPAT_API_URL = "http://localhost:8080/api/ccompat";
+    private static final String CCOMPAT_API_URL = "http://localhost:8080/apis/ccompat/v6";
 
     private static final String SERVERS = "localhost:9092";
     private static final String TOPIC_NAME = ConfluentSerdesExample.class.getSimpleName();
