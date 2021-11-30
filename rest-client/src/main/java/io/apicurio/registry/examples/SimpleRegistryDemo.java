aec476e674db9707157994866572f605398422e0
Carles Arnal <carlesarnal92@gmail.com>
2021-11-30 13:51:01 +0100

    Update rest client example

--- rest-client/src/main/java/io/apicurio/registry/examples/SimpleRegistryDemo.java
+++ rest-client/src/main/java/io/apicurio/registry/examples/SimpleRegistryDemo.java
@@ -1,10 +1,14 @@
 package io.apicurio.registry.examples;
 
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.UUID;
 
 import io.apicurio.registry.examples.util.RegistryDemoUtil;
 import io.apicurio.registry.rest.client.RegistryClient;
 import io.apicurio.registry.rest.client.RegistryClientFactory;
+import io.apicurio.rest.client.auth.OidcAuth;
 
 
 /**
@@ -23,7 +27,7 @@ public class SimpleRegistryDemo {
     static {
         // Create a Service Registry client
         String registryUrl = "http://localhost:8080/apis/registry/v2";
-        client = RegistryClientFactory.create(registryUrl);
+        client = createProperClient(registryUrl);
     }
 
     public static void main(String[] args) throws Exception {
@@ -38,5 +42,16 @@ public class SimpleRegistryDemo {
         RegistryDemoUtil.getSchemaFromRegistry(client, artifactId);
 
         RegistryDemoUtil.deleteSchema(client, artifactId);
+    }
+
+    public static RegistryClient createProperClient(String registryUrl) {
+        final String tokenEndpoint = System.getenv("AUTH_TOKEN_ENDPOINT");
+        if (tokenEndpoint != null) {
+            final String authClient = System.getenv("AUTH_CLIENT_ID");
+            final String authSecret = System.getenv("AUTH_CLIENT_SECRET");
+            return RegistryClientFactory.create(registryUrl, Collections.emptyMap(), new OidcAuth(tokenEndpoint, authClient, authSecret, Optional.empty()));
+        } else {
+            return RegistryClientFactory.create(registryUrl);
+        }
     }
 }
